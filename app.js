95ea71ed94aa4acc9d1b2787986babeac0d68cdc
Rob O'Callaghan <rob@robs-mbp.lan>
2020-07-08 20:00:06 +0100

    Cleanup Pre Business Logic

--- app.js
+++ app.js
@@ -2,6 +2,4 @@ const app = require('./src');
 
 const port = process.env.PORT || 8081;
 
-app.get('/', (req, res) => res.send('Hello World!')); // TODO Remove?
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/__specs__/skeleton.spec.js
+++ src/__specs__/skeleton.spec.js
@@ -3,12 +3,12 @@ const nock = require('nock');
 const app = require('..');
 
 describe('Walking Skeleton', () => {
-  describe('When the service is healthy', () => {
+  describe('When GET /ping', () => {
     beforeEach(() => {
       nock('http://google.com').get('/').reply(200);
     });
 
-    it('Should return 200 OK and send an outgoing HTTP GET', async () => {
+    it('Should return 200 OK and a String', async () => {
       await request(app).get('/ping').expect(200).expect('pong');
     });
   });
